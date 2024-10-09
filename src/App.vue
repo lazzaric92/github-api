cc4b1dcc373bd51f5a7dde56f6f94060eb07fdcb
lazzaric92 <lazzaric_92@hotmail.it>
2024-10-09 17:17:47 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<template>
+  <h1>Hello</h1>
+</template>
+
+<style lang="scss">
+@use '../node_modules/bootstrap/scss/bootstrap.scss';
+@use './assets/scss/general.scss';
+@use './assets/scss/partials/variables';
+@use './assets/scss/partials/mixins';
+</style>
