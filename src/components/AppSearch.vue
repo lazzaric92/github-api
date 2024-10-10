ab0a6500214cf2cc14d01995c263a8477c54d2d6
lazzaric92 <lazzaric_92@hotmail.it>
2024-10-10 15:56:40 +0200

    added search selector (repo/users)

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -6,20 +6,23 @@ export default{
         return {
             store,
             search: '',
+            searchSelect: '',
+            currentPage: 1,
         }
     },
     methods: {
-        getSearchedItems: function(searchString){
+        getSearchedItems: function(searchString, page){
             if(this.search.trim() != ''){
-                axios.get('https://api.github.com/search/repositories', {
+                axios.get(`https://api.github.com/search/${this.searchSelect}`, {
                     params: {
-                        q: searchString
+                        q: searchString,
+                        page: page,
                     }
                 })
                 .then((response) => {
                     console.log(response.data.items);
                     this.store.resultsArray = response.data.items;
-                    console.log(this.store.resultsArray);
+                    // console.log(this.store.resultsArray);
                 })
                 .catch((error) => {
                 console.log(error);
@@ -32,8 +35,12 @@ export default{
 
 <template>
     <nav class="d-flex justify-content-center">
+        <select class="text-center" v-model="searchSelect">
+            <option value="repositories" selected>Repositories</option>
+            <option value="users">Users</option>
+        </select>
         <input type="search" v-model="search" class="mx-2 ps-2" placeholder="Search">
-        <button class="btn btn-success" @click="getSearchedItems(search)">Search</button>
+        <button class="btn btn-success" @click="getSearchedItems(search, 1)">Search</button>
     </nav>
 </template>
 
